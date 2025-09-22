79a74f01d52e519cb91ee0303dd49dfaff29db48
Mazin Haggag <mazinhaggag@gmail.com>
2025-09-21 22:03:58 -0400

    new: added uoft specific validation for activity 1.4

--- hello.py
+++ hello.py
@@ -3,16 +3,17 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SECRET_KEY'] = 'secret'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your UofT Email address?', validators=[DataRequired(), Email()])
     submit = SubmitField('Submit')
 
 @app.route('/', methods=['GET', 'POST'])
@@ -22,10 +23,18 @@ def index():
         old_name = session.get('name')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+
         session['name'] = form.name.data
+        session['email'] = form.email.data
+
+        if not form.email.data.lower().endswith("@mail.utoronto.ca"):
+            session["name"] = form.name.data
+            session["email"] = None
+            flash("Please use your UofT email.")
+
         return redirect(url_for('index'))
     return render_template('index.html',
-        form = form, name = session.get('name'))
+        form = form, name = session.get('name'), email = session.get('email'))
 
 @app.route('/user/<name>')
 def user(name):
